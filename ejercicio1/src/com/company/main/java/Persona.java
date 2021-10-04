737b78e0f40fb0cf201e9b219e91c2abc141a122
Alicia Núñez <69097456+alicianunex@users.noreply.github.com>
2021-10-05 01:49:05 +0200

    Update Persona.java

--- ejercicio1/src/com/company/main/java/Persona.java
+++ ejercicio1/src/com/company/main/java/Persona.java
@@ -1,38 +1,27 @@
 package com.company.main.java;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Persona
 {
-    private String nombre;
-    private int telefono;
-    private short codigo;
+    private final String nombre;
+    private final int telefono;
+    private final short codigo;
 
 
     public String getNombre()
     {
         return this.nombre;
     }
-    public void setNombre(String nom) {
-        this.nombre = nom;
-    }
+
     public int getTelefono()
     {
         return this.telefono;
     }
-    public void setTelefono(int tlf)
-    {
-        this.telefono = tlf;
-    }
+
     public short getCodigo()
     {
         return this.codigo;
     }
-    public void setCodigo(short cod)
-    {
-        this.codigo = cod;
-    }
+
     public Persona(String nombre,int telefono, short codigo)
     {
         this.nombre=nombre;
