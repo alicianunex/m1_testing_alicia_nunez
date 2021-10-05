fb6facc441a1ab0567b49ebe187aaca69bb38378
Alicia Núñez <69097456+alicianunex@users.noreply.github.com>
2021-10-05 22:26:56 +0200

    Update Agenda.java

--- ejercicio1/src/com/company/main/java/Agenda.java
+++ ejercicio1/src/com/company/main/java/Agenda.java
@@ -14,26 +14,27 @@ public class Agenda
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in)); int opcion;
         while (true)
         {
-            mostrarMenu(); System.out.println("Introduzca una opcion"); opcion = Integer.parseInt(lector.readLine()); switch (opcion)
+            mostrarMenu(); System.out.println("Introduzca una opción"); opcion = Integer.parseInt(lector.readLine()); switch (opcion)
+
         {
             case 1:
                 String nom;
                 int tlf,cod;
-                System.out.println("Introduzca nombre:"); nom = lector.readLine(); System.out.println("Introduzca Telefono:"); tlf = Integer.parseInt(lector.readLine()); System.out.println("Introduzca Codigo:"); cod = Integer.parseInt(lector.readLine()); contacto=new Persona(nom,tlf,(short)cod); miagenda.addElement(contacto);
+                System.out.println("Introduzca nombre:"); nom = lector.readLine(); System.out.println("Introduzca Teléfono:"); tlf = Integer.parseInt(lector.readLine()); System.out.println("Introduzca Código:"); cod = Integer.parseInt(lector.readLine()); contacto=new Persona(nom,tlf,(short)cod); miagenda.addElement(contacto);
                 break;
             case 2:
                 for (int i=0; i< miagenda.size(); i++)
                 {
                     contacto = miagenda.elementAt(i);
-                    System.out.println("Contacto: "+contacto.getNombre()+", Telefono: " +contacto.getTelefono()+", Codigo: "+contacto.getCodigo());
+                    System.out.println("Contacto: "+contacto.getNombre()+", Teléfono: " +contacto.getTelefono()+", Código: "+contacto.getCodigo());
                 }
                 break;
             case 3:
                 int codigo;
                 String nombre;
                 int seleccion;
-                System.out.println("Seleccione una opcion de busqueda"); System.out.println("\n1.-Nombre");
-                System.out.println("2.- Codigo"); seleccion=Integer.parseInt(lector.readLine());
+                System.out.println("Seleccione una opción de búsqueda"); System.out.println("\n1.-Nombre");
+                System.out.println("2.- Código"); seleccion=Integer.parseInt(lector.readLine());
                 boolean encontrado = false;
                 switch (seleccion) {
                     case 1 -> {
@@ -47,13 +48,13 @@ public class Agenda
                             }
                         }
                         if (encontrado) {
-                            System.out.println("Contacto: " + contacto.getNombre() + ", Telefono: " + contacto.getTelefono() + ", Codigo: " + contacto.getCodigo());
+                            System.out.println("Contacto: " + contacto.getNombre() + ", Teléfono: " + contacto.getTelefono() + ", Código: " + contacto.getCodigo());
                         } else {
                             System.out.println("No se ha encontrado el contacto");
                         }
                     }
                     case 2 -> {
-                        System.out.println("Introduzca el codigo a buscar");
+                        System.out.println("Introduzca el código a buscar");
                         codigo = Integer.parseInt(lector.readLine());
                         for (int i = 0; i < miagenda.size(); i++) {
                             contacto = miagenda.elementAt(i);
@@ -63,24 +64,27 @@ public class Agenda
                             }
                         }
                         if (encontrado) {
-                            System.out.println("Contacto: " + contacto.getNombre() + ", Telefono: " + contacto.getTelefono() + ", Codigo: " + contacto.getCodigo());
+                            System.out.println("Contacto: " + contacto.getNombre() + ", Teléfono: " + contacto.getTelefono() + ", Código: " + contacto.getCodigo());
                         } else {
                             System.out.println("No se ha encontrado el contacto");
                         }
                     }
-                    default -> System.out.println("Opcion Incorrecta");
+                    default -> System.out.println("Option Incorrecta");
                 }
                 break;
             case 4:
                 System.exit(0);
                 break;
             default:
-                System.out.println("Opcion escrita incorrecta"); }
+                System.out.println("Opción escrita incorrecta");
+                case 5: System.out.println("5.- Borrar");
+            miagenda.clear();
+        }
         }
     }
     public static void mostrarMenu()
 
     {
-        System.out.println("\n1.-Insertar Contacto"); System.out.println("2.-Mostrar Contactos"); System.out.println("3.-Buscar Contacto"); System.out.println("4.-Salir\n");
+        System.out.println("\n1.-Insertar Contacto"); System.out.println("2.-Mostrar Contactos"); System.out.println("3.-Buscar Contacto"); System.out.println("4.-Salir");System.out.println("5.-Borrar Contactos\n");
     }
 }
